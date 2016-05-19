668dbd71e106f2d46e171ebf266f668e2c2c50c3
Pramod Sripada <pramod.sripada@gmail.com>
2016-05-19 19:10:22 -0400

    Setup basic server

---
+++ server.js
@@ -0,0 +1,13 @@
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+// Creating a server using a built in node module
+// Start a new server and use the express app as the boiler plate
+var http = require('http').Server(app);
+
+// use express static to expose a folder
+app.use(express.static(__dirname + '/public'));
+http.listen(PORT, function () {
+	console.log("Server started");
+});
+
