989d33d741e783188bd79d161ed93cc01e443946
Pramod Sripada <pramod.sripada@gmail.com>
2016-05-30 18:33:42 -0400

    Add styles for chat app

--- public/js/app.js
+++ public/js/app.js
@@ -18,10 +18,13 @@ socket.on('message', function (message) {
 	var timestampMoment = moment.utc(message.timestamp);
 	var localTimestamp = timestampMoment.local().format('h:mm a');
 
-	// Adding the message to the div - messages
-	var $message = jQuery(".messages")
-	$message.append("<p><strong>"+ message.name + ' ' + localTimestamp +"</strong></p>")
-	$message.append("<p>"+ message.text +"</p>")
+	// Adding the message to the ul - messages
+	var $messages = jQuery(".messages");
+	var $message = jQuery('<li class="list-group-item"></li>')
+
+	$message.append("<p><strong>"+ message.name + ' ' + localTimestamp +"</strong></p>");
+	$message.append("<p>"+ message.text +"</p>");
+	$messages.append($message);	
 });
 
 // Handles submitting of new message
